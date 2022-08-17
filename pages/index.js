2a982526c10acd58545fb6466545bb70897a32bd
Austin Frost <65328557+austinfroste@users.noreply.github.com>
2022-08-17 12:32:15 -0700

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Austin Frost</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 

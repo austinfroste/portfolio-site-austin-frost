76a6a2dc8bca70db7e010d677e901e8c7d7ee423
Austin Frost <65328557+austinfroste@users.noreply.github.com>
2022-08-17 12:05:47 -0700

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -6,11 +6,15 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Austin Frost</title>
+        <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
+        <Header title="Welcome to my app!" />
+        <p className="description">
+          Get started by editing <code>pages/index.js</code>
+        </p>
       </main>
 
       <body>
@@ -19,6 +23,8 @@ export default function Home() {
     	    <a href="https://github.com/austinfroste">GitHub</a> <a href="austindfrost@icloud.com">Email</a>
 	      </div>
       </body>
+
+      <Footer />
     </div>
   )
 }

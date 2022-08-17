ba04ef178b371f1867a042daeefe7efad7866fd2
Austin Frost <65328557+austinfroste@users.noreply.github.com>
2022-08-17 11:47:42 -0700

    updated layout and styles !

--- pages/index.js
+++ pages/index.js
@@ -6,15 +6,11 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Austin Frost</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
       </main>
 
       <body>

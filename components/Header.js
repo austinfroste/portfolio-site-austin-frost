290dc1fa264b2e857370df7392e01561d74ca105
Austin Frost <65328557+austinfroste@users.noreply.github.com>
2022-08-18 11:45:59 -0700

    Add files via upload

--- components/Header.js
+++ components/Header.js
@@ -2,6 +2,8 @@ import styles from './Header.module.css'
 
 export default function Header({ title }) {
   return (
-    <h1><a href="/">Austin Frost</a></h1>
+    <header>
+      <a href="/">Austin Frost</a>
+    </header>
   )
 }

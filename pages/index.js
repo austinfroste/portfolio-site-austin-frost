a969622fcec83b6b531e0df60d9892c4fbd95bff
Austin Frost <65328557+austinfroste@users.noreply.github.com>
2022-08-18 08:21:06 -0700

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import Footer from '@components/Footer'
 
 export default function Home() {
   return (
-    <div className="container">
+    <div /*className="container"*/>
       <Head>
         <title>@austinfroste | Austin Frost</title>
         <link rel="icon" href="/favicon.ico" />

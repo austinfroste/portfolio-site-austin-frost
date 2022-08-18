ac7dd87088904fb35a6003dc9f7c0b5332e49b94
Austin Frost <65328557+austinfroste@users.noreply.github.com>
2022-08-18 09:28:41 -0700

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -4,19 +4,13 @@ import Footer from '@components/Footer'
 
 export default function Home() {
   return (
-    <div className="main-content">
+    <div className="container">
       <Head>
         <title>@austinfroste | Austin Frost</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <body>
-        <div>
-          <h1><a href="/">Austin Frost</a></h1>
-        </div>
-
-        <hr></hr>
-
 	      <div>
 		      <p>Howdy, I'm <b>Austin Frost</b>, a third year CS student working in UO's Computational Social Neuroscience Lab. 
           If you have any inquiries, or would just like to chat, feel free to email! You can also check out my projects on GitHub. ✌️</p>
